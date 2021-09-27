c040c2aa1f2842c2e5d0ab46fde8b03ffd48e4f4
root <root@iPhone>
2021-09-27 17:55:31 -0400

    youtube video player public 2

--- src/app/components/yp/yp.component.ts
+++ src/app/components/yp/yp.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class YpComponent implements OnInit {
   player: YT.Player;
-  private id: string = 'DYF5FjMb9Cg';
+  public id: string = 'DYF5FjMb9Cg';
 
   savePlayer(player) {
     this.player = player;
